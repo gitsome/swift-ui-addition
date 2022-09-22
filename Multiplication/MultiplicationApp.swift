a4b890b9353936d5259df103319e0ce3df146845
John Martin <johndavidfive@gmail.com>
2022-09-22 14:56:35 -0700

    Initial Commit

---
+++ Multiplication/MultiplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplicationApp.swift
+//  Multiplication
+//
+//  Created by john martin on 9/22/22.
+//
+
+import SwiftUI
+
+@main
+struct MultiplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
